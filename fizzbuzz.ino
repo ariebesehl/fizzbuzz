58567e3a33380ecb52cb5f556637950a372a9d15
Andreas Riebesehl <a.riebesehl@gmail.com>
2024-08-17 13:21:39 +0000

    minor improvements, refactoring
    24230T113300UTC, out 398/36
    - feature: removed leading zero in digit output (+4 RAM)
    - optimization: removed redundant modulus check (-2 ROM)
    - refactor: globalized buffer (-40 ROM)

--- fizzbuzz.ino
+++ fizzbuzz.ino
@@ -1,22 +1,22 @@
 #include <util/delay.h>
 #define EINE_ZAHL 255
 static const unsigned char* gConst[3] = {(const unsigned char*)"Fizzbuzz!\n", (const unsigned char*)"Fizz!\n", (const unsigned char*)"Buzz!\n"};
-void gSerialInit(void) {UBRR0H = 0x0; UBRR0L = 0x0; UCSR0A = 0x0; UCSR0B = 0x8; UCSR0C = 0x6;} // 1M baud-rate, TX only 
+static unsigned char gBuffer[5] = {'\0', '\0', '\0', '\n', '\0'};
+void gSerialInit(void) {UBRR0H = 0x0; UBRR0L = 0x0; UCSR0A = 0x0; UCSR0B = 0x8; UCSR0C = 0x6;} // 1M baud-rate, RX only 
 void gSerialPrint(const unsigned char* iText) {unsigned char lChar; while ((lChar = *(iText)++)) {while (!(UCSR0A & 0x20)); UDR0 = lChar;}}
 int main(void) {
   gSerialInit();
-  unsigned char lBuffer[5] = {'\0', '\0', '\0', '\n', '\0'}, lCounter = 0, lState;
+  unsigned char lCounter = 0, lState;
   while (0x1) {
     ++lCounter;
     if ((lState = (lCounter % 3 ? 0x2 : 0x0) | (lCounter % 5 ? 0x1 : 0x0)) != 0x3) {
       gSerialPrint(gConst[lState]);
     } else {
-      unsigned char i = 3;
-      unsigned char lCache = lCounter;
-      do {lBuffer[--i] = 0x30 + (lCache % 10); lCache /= 10;} while (i);
-      gSerialPrint(lBuffer);
+      unsigned char lCache = lCounter, i = 3;
+      do {gBuffer[--i] = 0x30 + (lCache % 10);} while (lCache /= 10);
+      gSerialPrint(&gBuffer[i]);
     }
-    if (!(lCounter % EINE_ZAHL)) {lCounter = 0;};
+    lCounter %= EINE_ZAHL;
     _delay_ms(100);
   }
   return 0;
